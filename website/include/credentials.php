1f5cf614d30982b385e8119380e185fc4580b105
Sky4242 <42550513+Sky4242@users.noreply.github.com>
2018-11-29 13:35:10 -0500

    Update credentials.php

--- website/include/credentials.php
+++ website/include/credentials.php
@@ -3,10 +3,10 @@
 class Credentials {
 
     // Declare the credentials to the database
-    public $servername = "mysqlforlambdatest.cxhtq3lnkeyp.us-east-1.rds.amazonaws.com";
-    public $serverusername = "IT5236rduser";
-    public $serverpassword = "masteruser7";
-    public $serverdb = "IT5236DB";
+    public $servername = "*********************";
+    public $serverusername = "***************";
+    public $serverpassword = "****************";
+    public $serverdb = "*************";
 
 }
 
